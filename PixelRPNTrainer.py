ab8eec4994b1c93aca400b5e192be1869e91b82d
Michael Huang <michaelyhuang23@gmail.com>
2021-04-21 22:58:28 -0400

    first complete pixelRPN trainer

--- PixelRPNTrainer.py
+++ PixelRPNTrainer.py
@@ -154,14 +154,14 @@ adamOptimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
 bceLoss = tf.keras.losses.BinaryCrossentropy(
     reduction=tf.keras.losses.Reduction.SUM)
 
-
-@tf.function # need dimension optimization
+# need dimension optimization
+@tf.function(input_signature=[tf.TensorSpec(shape=(None,None,None,1), dtype=tf.float32),tf.TensorSpec(shape=(None,None,None,1), dtype=tf.float32),tf.TensorSpec(shape=(None,None,None,1), dtype=tf.float32),tf.TensorSpec(shape=(None,None,None,1), dtype=tf.float32),tf.TensorSpec(shape=(None,None,None,1), dtype=tf.float32),tf.TensorSpec(shape=(None,None,None), dtype=tf.float32),tf.TensorSpec(shape=(None,None,None), dtype=tf.float32),tf.TensorSpec(shape=(None,None,None), dtype=tf.float32),tf.TensorSpec(shape=(None,None,None), dtype=tf.float32),tf.TensorSpec(shape=(None,None,None), dtype=tf.float32)])
 def loss_binary_crossentropy(pred0, pred1, pred2, pred3, pred4, label0, label1, label2, label3, label4):
-    lossSum = bceLoss(pred0[:, :, :, 0], label0)
-    lossSum += bceLoss(pred1[:, :, :, 0], label1)
-    lossSum += bceLoss(pred2[:, :, :, 0], label2)
-    lossSum += bceLoss(pred3[:, :, :, 0], label3)
-    lossSum += bceLoss(pred4[:, :, :, 0], label4)
+    lossSum = bceLoss(pred0[:, :, :, 0], label0)/tf.cast(tf.size(label0),tf.float32)
+    lossSum += bceLoss(pred1[:, :, :, 0], label1)/tf.cast(tf.size(label1),tf.float32)
+    lossSum += bceLoss(pred2[:, :, :, 0], label2)/tf.cast(tf.size(label2),tf.float32)
+    lossSum += bceLoss(pred3[:, :, :, 0], label3)/tf.cast(tf.size(label3),tf.float32)
+    lossSum += bceLoss(pred4[:, :, :, 0], label4)/tf.cast(tf.size(label4),tf.float32)
     return lossSum
 
 
@@ -179,15 +179,24 @@ def train_step(img, label0, label1, label2, label3, label4):
         zip(gradients, ProposerModel.trainable_variables))
 
 
-lossAvg = 0
-for i, val_data in enumerate(val_dataset):
-    objectnesses = ProposerModel(val_data[0])
-    loss = loss_binary_crossentropy(*objectnesses, *val_data[1:])
-    lossAvg = (lossAvg*i+loss)/(i+1)
-    sys.stdout.write("evaluating: %d/%d" %
-                         (i, val_size))
-    sys.stdout.flush()
-print(f'initial loss value is {lossAvg}')
+@tf.function(input_signature=[tf.TensorSpec(shape=(None, None, None, 3), dtype=tf.float32), tf.TensorSpec(shape=(None, None, None), dtype=tf.float32), tf.TensorSpec(shape=(None, None, None), dtype=tf.float32), tf.TensorSpec(shape=(None, None, None), dtype=tf.float32), tf.TensorSpec(shape=(None, None, None), dtype=tf.float32), tf.TensorSpec(shape=(None, None, None), dtype=tf.float32)])
+def eval_test(img, label0, label1, label2, label3, label4):
+    objectnesses = ProposerModel(img)
+    loss = loss_binary_crossentropy(*objectnesses, label0, label1, label2, label3, label4)
+    return loss
+
+
+def eval(dataset, size):
+    lossAvg = 0
+    for i, data in enumerate(dataset):
+        loss = eval_test((*data))
+        lossAvg = (lossAvg*i+loss)/(i+1)
+        sys.stdout.write("evaluating: %d/%d\r" %
+                            (i, size))
+        sys.stdout.flush()
+    return lossAvg
+
+print(f'initial loss value is {eval(val_dataset,val_size)}')
 
 Epoch = 1
 for epoch in range(Epoch):
@@ -201,13 +210,6 @@ for epoch in range(Epoch):
         sys.stdout.write("training: %d/%d    time per batch: %f \r" %
                          (i, train_size, time.time()-st))
         sys.stdout.flush()
-    lossAvg = 0
-    for i, val_data in enumerate(val_dataset):
-        objectnesses = ProposerModel(val_data[0])
-        loss = loss_binary_crossentropy(*objectnesses, *val_data[1:])
-        lossAvg = (lossAvg*i+loss)/(i+1)
-        sys.stdout.write("evaluating: %d/%d" %
-                         (i, val_size))
-        sys.stdout.flush()
+
     print(f'epoch {epoch+1} is finished')
-    print(f'loss value is {lossAvg}')
+    print(f'loss value is {eval(val_dataset,val_size)}')
