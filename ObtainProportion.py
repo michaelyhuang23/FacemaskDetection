cac4870ce8dfc19cecc446ed92b8d58c5a35ac7a
Michael Huang <michaelyhuang23@gmail.com>
2021-04-25 00:42:26 -0400

    created ObtainProportion to scale loss value by proportion between cases of false negative and false positive

---
+++ ObtainProportion.py
@@ -0,0 +1,16 @@
+from DataReader import read_data
+import tensorflow as tf
+
+train_dataset, val_dataset, train_size, val_size = read_data('Data/imgs_train.npy', 'Data/data_boxes_train.txt', 'Data/data_sizes_train.txt', 0.2)
+
+avgRoll = 0
+for i,train_data in enumerate(train_dataset):
+    img, label0, label1, label2, label3, label4 = train_data
+    prop0 = tf.cast(tf.math.count_nonzero(label0), tf.int32)/tf.size(label0)
+    prop1 = tf.cast(tf.math.count_nonzero(label1), tf.int32)/tf.size(label1)
+    prop2 = tf.cast(tf.math.count_nonzero(label2), tf.int32)/tf.size(label2)
+    prop3 = tf.cast(tf.math.count_nonzero(label3), tf.int32)/tf.size(label3)
+    prop4 = tf.cast(tf.math.count_nonzero(label4), tf.int32)/tf.size(label4)
+    avgRoll=(avgRoll*i+(prop0+prop1+prop2+prop3+prop4)/5)/(i+1)
+
+print(avgRoll)
--- test.py
+++ test.py
@@ -1,12 +1,6 @@
 import tensorflow as tf
 import time
 
-a = tf.data.Dataset.range(1,4)
-b = tf.data.Dataset.range(4,7)
-c = tf.data.Dataset.range(7,10)
-
-d = tf.data.Dataset.zip((a,b))
-
-e = tf.data.Dataset.zip((c,*d))
-
-print(list(e.as_numpy_iterator()))
+a = tf.constant(3,dtype=tf.int32)
+b = tf.constant(2,dtype=tf.int32)
+print(a/b)
